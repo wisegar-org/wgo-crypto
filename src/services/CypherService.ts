921e11fa9545e3c305d0a81be46cd98ff75e0edc
Yariel  Rodriguez <info@wisegar.org>
2022-04-18 17:41:46 -0400

    Fix export only cypherData and decypherData on CypherService class

--- src/services/CypherService.ts
+++ src/services/CypherService.ts
@@ -2,34 +2,40 @@ import { ICypherConfigInput } from "../interfaces/ICypherConfig";
 import {
   cypherDataByConfig,
   decypherDataByConfig,
-  getCypherDataByConfig,
   getCypherDataConfig,
-  getCypherDataInitVectorByConfig,
 } from "./CypherFunctions";
+
+/**
+ * CypherService class
+ */
 export class CypherService {
   private readonly cypherKey: string;
   private readonly configObj: ICypherConfigInput;
   /**
-   *
+   * Cypher Service constructor.
+   * @param cypherKey Need to be a string to cypher data.
+   * @param configObj ICypherConfigInput with the config. This param is optional.
    */
   constructor(cypherKey: string, configObj?: ICypherConfigInput | null) {
     this.cypherKey = cypherKey;
     this.configObj = getCypherDataConfig(configObj);
   }
 
-  cypherData(data: any) {
+  /**
+   * Data cypher
+   * @param data Can be any base object ex. {userid: 1, expiration: '2234556', sessionid: 'nwljkcnlndclwnwle'}
+   * @returns string
+   */
+  cypherData(data: any): string {
     return cypherDataByConfig(data, this.cypherKey, this.configObj);
   }
 
-  decypherData(cypherdata: string) {
+  /**
+   * Data decypher
+   * @param cypherdata Most a already cyphered data
+   * @returns json data object
+   */
+  decypherData(cypherdata: string): any {
     return decypherDataByConfig(cypherdata, this.cypherKey, this.configObj);
-  }
-
-  getCypherData(cypherdata: string) {
-    return getCypherDataByConfig(cypherdata, this.configObj);
-  }
-
-  getCypherDataInitVector(cypherdata: string) {
-    return getCypherDataInitVectorByConfig(cypherdata, this.configObj);
   }
 }
